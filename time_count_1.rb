f1a886693117773252826c89c963bf6c68b86d18
SPBobrov <spb.74@mail.ru>
2025-01-24 22:43:55 +0300

    Свел воедино accounting_ten.rb b time_count_1.rb

--- time_count_1.rb
+++ time_count_1.rb
@@ -1,14 +1,20 @@
 #Реализовать: передачу учтенного времени в хеш, запись хеша в txt
 #Реализовать: передачу учтенного времени в программу accounting_ten.rb
+#На самом деле нужно объединить программы
+#Нужно сделать округление данные вносимых в хеш
+
+####################################
+#Методы
+####################################
 
 
 def time_fixing()
-    Time.now.to_f
+  Time.now.to_i #поменял с .to_f для секунд не нужно это
 end
 
 def write_to_txt(adress, data)
-    file = File.new(adress, "a:UTF-8")
-    file.print(data)
+  file = File.new(adress, "a:UTF-8")
+  file.print(data)
 	file.close
 end
 
@@ -47,49 +53,84 @@ def pty_to_hs (file_adress)
 	return hs
 end
 
+####################################
+#Основной цикл
+####################################
+
+####################################
+#Переменные
+####################################
+
+log = "./day_log.txt"
+
+####################################
+#Основная программа
+####################################
+
+accounting_ten_1 = pty_to_hs(log)
+
+puts "Результары на сегодня: ruby - #{accounting_ten_1['ruby']}, git -
+ #{accounting_ten_1['git']}, linux - #{accounting_ten_1['linux']}, english
+  - #{accounting_ten_1['english']}, tools - #{accounting_ten_1['tools']} "
+
+accounting_ten_1_sum = 0 # обнуляем начальное значение, чтобы посчитать сумму часов на начало учета
+accounting_ten_1.each do |key, value|
+	accounting_ten_1_sum += value
+end
+
+puts "Всего #{accounting_ten_1_sum.round(2)}"
+
+puts "Осталось до цели #{10000 - accounting_ten_1_sum} "
+
+print 'Готовы начать учет? Да/нет '
+is_new_data = gets.chomp.downcase
+
+if is_new_data == 'да' || is_new_data == 'yes'
+
+
+
 loop do
 
-print 'Выберите объект учета ruby - 1, english - 2, linux - 3, git - 4, tools - 5 '
-obj_count = gets.chomp.to_i
+  print 'Выберите объект учета ruby - 1, english - 2, linux - 3, git - 4, tools - 5, закончить учет - 6 '
+  obj_count = gets.chomp.to_i
 
-time_start = time_fixing #начинаем учет выбранного
+  time_start = time_fixing #начинаем учет выбранного
 
-if obj_count == 1
-    log_adress = "./ruby.txt"
+
+  if obj_count == 1
+
     obj_count = 'ruby'
-elsif obj_count == 2
-    log_adress = "./english.txt"
+  elsif obj_count == 2
     obj_count = 'english'
-elsif obj_count == 3
-    log_adress = "./linux.txt"
+  elsif obj_count == 3
     obj_count = 'linux'
-elsif obj_count == 4
-    log_adress = "./git.txt"
+  elsif obj_count == 4
     obj_count = 'git'
-elsif obj_count == 5
-    log_adress = "./tools.txt"
+  elsif obj_count == 5
     obj_count = 'tools'
-else
+  elsif obj_count == 6
     exit
-end
+  else
+    exit
 
-write_to_txt(log_adress, time_start) #записываю начало времени учета в файл
+  end
 
-print 'Остановить учет? Да/нет'
-wish = gets.chomp.downcase
 
-if wish == 'да' || wish == 'yes'
+  print 'Остановить учет позиции? Да/нет '
+
+  wish = gets.chomp.downcase
+
+  if wish == 'да' || wish == 'yes'
+
     time_end = time_fixing #останавливаем учет выбранного
-    time_result = (time_end - time_start)/60/60
-    day_hs = pty_to_hs ("./day_log.txt")
-    day_hs[obj_count] += time_result.round(2)
-    pty_to_txt(day_hs, "./day_log.txt")
+    time_result = (time_end - time_start) / 60.0 / 60.0
+    day_hs = pty_to_hs (log)
+    day_hs[obj_count] += time_result.round(2) #округляется значение, которое попадет в хеш
+    pty_to_txt(day_hs, log)
     puts "Вы работали #{time_result.round(2)} часов"
-    write_to_txt(log_adress, time_end) #записываю окончание учета в файл
-end
-
-
-
+    
+  end
 
 end
 
+end
